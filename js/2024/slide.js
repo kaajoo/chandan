22a76bb02bb178654b8ae5ebe39468ebd77ff222
Chandan Chaurasia <cchaurasia@gmail.com>
2025-01-06 09:43:37 +0545

    l

--- js/2024/slide.js
+++ js/2024/slide.js
@@ -1,37 +1,20 @@
-
-window.onscroll = function() {
-    var wrapper = document.querySelector('body');
-    var textSlides = document.querySelectorAll('.slide .text');
-
-    for (var i = 0; i<textSlides.length; i++) {
-        var textSlide = textSlides[i];
-        var visible = checkVisible(textSlide);
-        if (visible) {
-            textSlide.parentElement.classList.add('on');
-        } else {
-            textSlide.parentElement.classList.remove('on');
-        }
-    }
-};
-
-
 function checkVisible(elm) {
-  var rect = elm.getBoundingClientRect();
-  // var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-  var viewHeight = window.innerHeight;
-  return !(rect.bottom < 0 || rect.top - viewHeight >= -viewHeight/2);
+    var rect = elm.getBoundingClientRect();
+    // var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    var viewHeight = window.innerHeight;
+    return !(rect.bottom < 0 || rect.top - viewHeight >= -viewHeight / 2);
 }
 
 function hamburgler() {
     var ham = document.querySelector('.gud-style__hamburger');
     var overlay = document.querySelector('.gud-style__menu_overlay')
     var section = document.getElementsByClassName('nav-link');
-    ham.addEventListener('click', function(){
+    ham.addEventListener('click', function() {
         this.classList.toggle('open');
         overlay.classList.toggle('open');
     });
-    for(i=0; i < section.length; i++) {
-        section[i].addEventListener('click', function(){
+    for (i = 0; i < section.length; i++) {
+        section[i].addEventListener('click', function() {
             console.log(this);
             ham.classList.toggle('open');
             overlay.classList.toggle('open');
